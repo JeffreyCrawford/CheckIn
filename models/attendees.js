152bb4ecd1a08cabfde4e8a167e16b5c7f9abb6c
Jeff Crawford <jeffrey.a.crawford@gmail.com>
2018-10-15 15:36:32 -0400

    transfer

--- models/attendees.js
+++ models/attendees.js
@@ -1,47 +1,123 @@
-module.exports = function(sequelize, Sequelize) {
-    const Attendee = sequelize.define("attendee", {
 
-        name: {
-            type: Sequelize.STRING,
-            allowNull: true
-        },
 
-        title: {
-            type: Sequelize.STRING,
-            allowNull: true
-        },
-
-        community: {
-            type: Sequelize.STRING,
-            allowNull: true
-        },
-
-        county: {
-            type: Sequelize.STRING,
-            allowNull: true
-        },
-
-        phone: {
-            type: Sequelize.STRING,
-            allowNull: true
-        },
-
-        email: {
-            type: Sequelize.STRING,
-            allowNull: true
-        },
-
-        votingMember: {
-            type: Sequelize.BOOLEAN,
-            allowNull: true
-        },
-
-        checkInTime: {
-            type: Sequelize.DATE,
-            allowNull: true
-        },
-    
-    });
-
-    return Attendee;
-}
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define('attendees', {
+		Contact: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Contact'
+		},
+		RowChecksum: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Row Checksum'
+		},
+		ModifiedOn: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Modified On'
+		},
+		fullName: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: ' Full Name'
+		},
+		firstName: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'First Name'
+		},
+		middleName: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Middle Name'
+		},
+		lastName: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Last Name'
+		},
+		gaDelegate: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'GA Delegate'
+		},
+		mailingPrefix: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Mailing Prefix'
+		},
+		account: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Account'
+		},
+		county: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'County'
+		},
+		salutation: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Salutation'
+		},
+		jobTitle: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Job Title'
+		},
+		email: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Email'
+		},
+		nopecGeneralAssemblyMember: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Nopec General Assembly Member'
+		},
+		address1Street1: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Address 1: Street 1'
+		},
+		address1Street2: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Address 1: Street 2'
+		},
+		address1City: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Address 1: City'
+		},
+		address1StateProvince: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Address 1: State/Province'
+		},
+		address1ZipPostalCode: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Address 1: ZIP/Postal Code'
+		},
+		owner: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Owner'
+		},
+		gaVotingRights: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'GA Voting Rights'
+		},
+		primaryContact: {
+			type: DataTypes.TEXT,
+			allowNull: true,
+			field: 'Primary Contact'
+		}
+	}, {
+		tableName: 'attendees'
+	});
+};
